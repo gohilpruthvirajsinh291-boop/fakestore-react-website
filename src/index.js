545e63d537eac6def047d97714fdfe27bb6002db
Pruthvirajsinh Gohil <gohilpruthvirajsinh291@gmail.com>
2026-03-10 11:54:43 +0530

    fix products components

--- src/index.js
+++ src/index.js
@@ -10,15 +10,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/fakestore-react-website">
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
